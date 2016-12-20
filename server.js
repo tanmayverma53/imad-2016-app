d2d36824e260afae4e5da648eef9a313d6c20aef
tanmayverma53 <tanmayverma080598@gmail.com>
2016-12-20 22:11:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/rahul.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rahul.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
